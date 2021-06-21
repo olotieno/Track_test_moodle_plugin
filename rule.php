b345eae6c1f14239c7aa027dde62a62cd2802ef2
Oliver Lando <oliverlandootieno@gmail.com>
2021-06-21 19:47:45 +0300

    Update rule.php

--- rule.php
+++ rule.php
@@ -14,13 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Implementaton for the quizaccess_proctoring plugin.
- *
- * @package    quizaccess_proctoring
- * @copyright  2020 Brain Station 23
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+
 
 
 defined('MOODLE_INTERNAL') || die();
